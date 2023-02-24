6ac4e22995c2b319811ca7603fa0b1f2ed6092fe
Vishal Sonthalia <vishals123@gmail.com>
2023-02-24 16:50:12 -0600

    Rename file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ItemModal from './ItemModal/ItemModal';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <ItemModal/>
         </a>
       </header>
     </div>
---
+++ src/ItemModal/ItemModal.js
@@ -0,0 +1,6 @@
+const ItemModal = () => {
+
+    console.log('item modal')
+}
+
+export default ItemModal;
