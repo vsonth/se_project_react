2111805abdc983529a10c7199a48c2ba15dea4b5
Vishal Sonthalia <vishals123@gmail.com>
2023-03-08 21:52:47 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,73 @@ import "./App.css";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./footer/Footer";
+import ModalWithForm from "./ModalWithForm/ModalWithForm";
+import { useEffect, useState } from "react";
+import ItemModal from "./ItemModal/ItemModal";
+import { getForecastWeather, parseWeatherData } from "./util/weatherApi";
 
 function App() {
   const weatherTemp = "102°F";
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const [temp, setTemp] = useState(0)
+
+  const handleCreateModal = () => {
+    setActiveModal("create");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
+  const handleSelectedCard = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  useEffect(() => {
+    getForecastWeather().then((data) => {
+      const temperature = parseWeatherData(data);
+      setTemp(temperature)
+    });
+  }, []);
+  console.log(temp)
+
   return (
     <div>
-      <Header />
-      <Main weatherTemp={weatherTemp} />
+      <Header onCreateModal={handleCreateModal} temp={temp}/>
+      <Main weatherTemp={temp} onSelectCard={handleSelectedCard} />
       <Footer />
+      {activeModal === "create" && (
+        <ModalWithForm title="New Garment" onClose={handleCloseModal}>
+          <label>
+            Name
+            <input type="text" name="name" minLength="1" maxLength="30" />
+          </label>
+          <label>
+            Image
+            <input type="url" name="link" minLength="1" maxLength="30" />
+          </label>
+          <p>Select the weather type:</p>
+          <div>
+            <div>
+              <input type="radio" id="hot" value="hot" />
+              <label> Hot</label>
+            </div>
+            <div>
+              <input type="radio" id="warm" value="warm" />
+              <label> Warm</label>
+            </div>
+            <div>
+              <input type="radio" id="cold" value="cold" />
+              <label> Cold</label>
+            </div>
+          </div>
+        </ModalWithForm>
+      )}
+      {activeModal === "preview" && (
+        <ItemModal selectedCard={selectedCard} onClose={handleCloseModal} />
+      )}
     </div>
   );
 }
--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -1,6 +1,18 @@
-const ItemModal = () => {
+const ItemModal = ({ selectedCard ,onClose }) => {
+  console.log("item modal");
 
-    console.log('item modal')
-}
+  return (
+    <div className={`modal`}>
+      <div className="modal__content">
+      <button type="button" onClick={onClose}>
+          Close
+        </button>
+        <img src={selectedCard.link} />
+        <div> {selectedCard.name}</div>
+        <div> Weather type: {selectedCard.weather}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ItemModal;
